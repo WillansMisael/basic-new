475db83d47553bdd82ad5b41e5322e0fd3f5c04b
WillansMisaelLinux <willansmisaelromerocondori@gmail.com>
2020-04-01 20:48:34 -0400

    Implementacion del formulario de creacion

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -29,7 +29,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        //retornamos la vista
+        return view('posts.create');
     }
 
     /**
